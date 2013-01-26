22bc061a5982157852e2cb4c63f74e1411446cd7
Maël Nison <nison.mael@gmail.com>
2013-01-26 18:40:35 -0500

    Updates environment

--- src/namespace.js
+++
@@ -1,1 +0,0 @@
-var PERLIN = Object.create( null );
--- utils/build.js
+++ utils/build.js
@@ -1,39 +1,12 @@
 var fs = require( 'fs' );
 
-var parser = require( 'uglify-js' ).parser;
-var uglify = require( 'uglify-js' ).uglify;
+var Glob = require( 'glob' );
+var UglifyJS = require( 'uglify-js' );
 
-var getSourceCode = function ( list ) {
+var uglifySourceCode = function ( sources ) {
+    return UglifyJS
+        .minify( sources, {
+            compress : true } ).code; };
 
-	return list.map( function ( path ) {
-
-		return fs.readFileSync( path, 'utf8' );
-
-	} ).join( '\n' );
-
-};
-
-var uglifySourceCode = function ( sourceCode ) {
-
-	var ast = parser.parse( sourceCode );
-
-	ast = uglify.ast_mangle( ast );
-	ast = uglify.ast_squeeze( ast );
-
-	return uglify.gen_code( ast );
-
-};
-
-var sourceCode = getSourceCode( [
-
-	'src/namespace.js',
-
-	'src/Core/Generator.js'
-
-] );
-
-fs.writeFile( 'build/Perlin.js',
-	uglifySourceCode(
-		sourceCode
-	)
-);
+fs.writeFileSync( 'build/Perlin.js',
+    uglifySourceCode( Glob.sync( 'src/**/*.js' ) ) );
